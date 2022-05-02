ef95202daf4d9c1c68f0253c138b56a3d0c88e43
nphbo1998@gmail.com <nph123312213>
2022-05-02 18:02:12 +0700

    third commit

--- src/components/SvgBlock/index.js
+++ src/components/SvgBlock/index.js
@@ -1,30 +1,7 @@
-import React from "react";
-import styled from "styled-components";
+// This is SvgBlock component, It will render svgs like in the services section.
 
-const Rb = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 40%;
-  position: relative;
-  /* z-index: 10; */
-  svg {
-    width: 100%;
-    height: auto;
-  }
-  @media only Screen and (max-width: 48em) {
-    display: none;
-  }
-`;
-
-const SvgBlock = ({ svg }) => {
-  const SvgIcon = require(`../../assets/${svg}`).default;
-  //console.log(SvgIcon);
-  return (
-    <Rb id="svgBlock">
-      <img src={SvgIcon} alt="Services" />
-    </Rb>
-  );
+const SvgBlock = () => {
+  return <div>SvgBlock Component</div>;
 };
 
 export default SvgBlock;
